ec85321b34d88a7bc909d72eb5c4a68b6a3fc101
John <John@LAPTOP-6JDSB2DM>
2023-01-29 17:00:18 -0700

    Finished age calculator created a java web and coded the age calculator missing href for referencing arithmetic calculator.

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author JohnC. <john.crudo@edu.sait.ca>
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String inputage = request.getParameter("inputage");
+        request.setAttribute("inputage", inputage);
+
+        if (inputage == null || inputage.equals("")) {
+            request.setAttribute("popmsg", "You must give your current age.");
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+            return;
+        }
+
+        try {
+            int nextAge = Integer.parseInt(inputage);
+            nextAge++;
+            request.setAttribute("popmsg", "Your age next birthday will be " + nextAge);
+        } catch (NumberFormatException e) {
+            
+            request.setAttribute("popmsg", "You must give a number.");
+            return;
+        }
+
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp")
+                .forward(request, response);
+    }
+
+}
---
+++ web/WEB-INF/ageCalculator.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : ageCalculator
+    Created on : 29-Jan-2023, 4:14:57 PM
+    Author     : JohnC. <john.crudo@edu.sait.ca>
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form method="post" action="age">
+            Enter your age: <input type="number" name="inputage" value="${inputage}" max="150">
+            <br>
+            <input type="submit" value="Age next birthday">
+        </form>
+        <p>${popmsg}</p>
+    </body>
+</html>
